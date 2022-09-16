1da692717e88b382b6624a4dc89a9a0ba2bf1b9e
nakhoa <nakhoa@nike.local>
2022-09-16 16:41:19 +0700

    feat: update histoire

--- .histoire.js
+++ .histoire.js
@@ -1,5 +1,9 @@
 import { defineConfig } from 'histoire'
+import { HstVue } from '@histoire/plugin-vue'
 
 export default defineConfig({
-  setupFile: './histoire.setup.ts'
+  setupFile: './histoire.setup.ts',
+  plugins: [
+    HstVue(),
+  ],
 })
